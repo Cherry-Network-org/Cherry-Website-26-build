a3901905f882fc8253869954d2d417b4e300384a
cherry-network-org <team.cherrynetwork@gmail.com>
2026-08-04 13:35:56 +0530

    Update gallery with compressed images

--- src/data/gallery.ts
+++ src/data/gallery.ts
@@ -72,7 +72,7 @@ export const galleryEvents: GalleryEvent[] = [
     title: "The Last",
     highlight: "Commit",
     photos: files("TheLastCommit", "The Last Commit '26", [
-      "tlc1.jpg",
+      "tlc1.JPG",
       "tlc3.jpg",
       "tlc4.jpg",
       "tlc5.jpg",
